e342b3ea84f6dd40f824d839a917ed53195449fe
teastwood3317 <teastwood3317@gmail.com>
2026-09-11 13:43:31 -0700

    Added Logo to tab image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Abrigar',
   tagline: 'Welcome, wandering Soul',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/Abrigar Logo.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
